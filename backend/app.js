0e49503e56de33d9e2ae55adc84e64ba94aa4895
Cecile <cecile.courrat@gmail.com>
2022-01-31 16:51:17 +0100

    debut backend

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const { Sequelize }  = require('sequelize');
+
+const sequelize = new Sequelize("database", "root", "10011982CC", {
+    dialect: "mysql",
+    host: "localhost"
+});
+
+try {
+    sequelize.authenticate();
+    console.log('Connecté à la base de données MySQL!');
+  } catch (error) {
+    console.error('Impossible de se connecter, erreur suivante :', error);
+}
+
+const app = express();
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
---
+++ backend/controllers/user.js
@@ -0,0 +1,6 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+const fs = require('fs');
+
